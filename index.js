8df5e7660d886d37d4bdc7b5ac03fb86c712ceaf
Muhammet Safa Sak <msafasak@gmail.com>
2023-07-09 12:44:33 +0300

    yanlis oldu

--- index.js
+++ index.js
@@ -25,8 +25,37 @@ function updateClock() {
     clockElement.textContent = formattedTime;
   }
 
-  
+  setInterval(updateClock)  ;
 
-  // Saati güncellemek için interval kullanarak updateClock() fonksiyonunu çağırın
-   setInterval(updateClock)  ;
-  
+
+
+  // Buton öğesini seçin
+var button = document.getElementById("bubbleButton");
+
+// Butona tıklanma olayını dinleyin
+button.addEventListener("click", createBubble);
+
+// Bubble oluşturma işlevi
+function createBubble() {
+  // Yeni bir div öğesi oluşturun ve stili ayarlayın
+  var bubble = document.createElement("div");
+  bubble.classList.add("bubble");
+
+  // Bubble'ın rastgele konumda görünmesini sağlayın
+  var randomX = Math.floor(Math.random() * window.innerWidth);
+  var randomY = Math.floor(Math.random() * window.innerHeight);
+  bubble.style.left = randomX + "px";
+  bubble.style.top = randomY + "px";
+
+  // Bubble'ı container öğesine ekleyin
+  var container = document.getElementById("container");
+  container.appendChild(bubble);
+
+  // Belirli bir süre sonra bubble'ı silmek için zamanlayıcı kullanın
+  setTimeout(function () {
+    container.removeChild(bubble);
+  }, 500);
+}
+
+
+// not: bunu ben yapmadım maalesef uğraştım ancak olmadı. chat gpt sağ olsun.
